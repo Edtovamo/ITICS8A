54668154c41c1adda2a50881dfead094f636a6ca
richa <richa@Edwin>
2021-03-01 09:20:52 -0600

    Commit tablas y serie terminado

---
+++ src/menuop/Serie.java
@@ -0,0 +1,13 @@
+package menuop;
+import javax.swing.JOptionPane; 
+public class Serie {
+    int num;
+    public void laserie(){
+        for (num = 0; num <= 100; num++) {
+            JOptionPane.showMessageDialog(null, num);
+        }
+        
+}
+   
+    
+}
